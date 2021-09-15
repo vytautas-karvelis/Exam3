e296cad335ebd1008decf92279ee485eeaca1b80
vytautas-karvelis <vytkarvelis@gmail.com>
2021-09-15 18:14:43 +0200

    make it better

--- app.js
+++ app.js
@@ -26,26 +26,25 @@ app.get('/api/jokes/', async (req, res)=>{
     res.json(jokes)
 })
 
-app.get('/api/jokes/:amount', async (req, res)=>{
-   
+app.get('/api/jokes/:combo', async (req, res)=>{
+    
+    if(isNaN(req.params.combo)){
+        let jokes = []
+        for(let i =0; i <10; i++){
+            const response = await fetch(`https://api.chucknorris.io/jokes/random?category=${req.params.combo}`);
+            const data = await response.json();
+            jokes.push(data.value)
+        }
+        return res.json(jokes)
+    }
     let jokes = []
-    for(let i =0; i <req.params.amount; i++){
+    for(let i =0; i <req.params.combo; i++){
         const response = await fetch('https://api.chucknorris.io/jokes/random');
         const data = await response.json();
         jokes.push(data.value)
         console.log(data)
     }
-    res.json(jokes)
-})
-
-app.get('/api/jokes/c/:category', async (req, res)=>{
-    let jokes = []
-    for(let i =0; i <10; i++){
-        const response = await fetch(`https://api.chucknorris.io/jokes/random?category=${req.params.category}`);
-        const data = await response.json();
-        jokes.push(data.value)
-    }
-    res.json(jokes)
+    return res.json(jokes)
 })
 
 app.get('/api/jokes/:category/:amount', async (req, res)=>{
